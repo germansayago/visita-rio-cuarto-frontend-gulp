7a60bb61633e6968a43a6bcfb7c5c938c489b3b4
cmblue <german.sayago@gmail.com>
2019-11-05 12:59:29 -0300

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,49 @@
+// ## count
+//
+$('.count').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 1000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
+});
+
+$(document).ready(function(){
+
+  // $("#form").hide();
+  $("#iconTimes").hide();
+  //
+  $("#buttonForm").click(function(){
+    $("#form").toggle();
+    $("#logo").toggle();
+    $("#iconSearch").toggle();
+    $("#iconTimes").toggle();
+  });
+
+});
+
+(function($) {
+  
+  "use strict";  
+
+  $(window).on('load', function() {
+
+     
+    /* WOW Scroll Spy
+    ========================================================*/
+     var wow = new WOW({
+      //disabled for mobile
+        mobile: false
+    });
+
+    wow.init();    
+  
+
+  });      
+
+}(jQuery));
+
---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+var gulp = require('gulp');
+
+// required plugins
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify'); // min js
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano'); // min css
+var del = require('del');
+var runSequence = require('run-sequence');
+var nunjucksRender = require('gulp-nunjucks-render'); // template engine
+
+// directorio de desarrollo
+var src = 'app';
+// directorio de produccion
+var dist = 'dist';
+
+// browserSync
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: src
+    },
+  })
+})
+
+// sass
+gulp.task('sass', function(){
+  return gulp.src(src + '/scss/**/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest(src + '/css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+})
+
+// template engine nunjukcs => html
+gulp.task('nunjucks', function() {
+  return gulp.src(src + '/pages/**/*.+(html|njk|nunjucks)')
+    .pipe(nunjucksRender({
+      path: [src + '/templates/']
+    }))
+    .pipe(gulp.dest(src))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+// images
+gulp.task('images', function() {
+  return gulp.src([src + '/images/*'])
+          .pipe(gulp.dest(dist + '/images/'));
+});
+
+// fonts
+gulp.task('fonts', function() {
+  return gulp.src([src + '/webfonts/*'])
+          .pipe(gulp.dest(dist + '/webfonts/'));
+});
+
+// watch
+gulp.task('watch',['browserSync', 'sass'], function(){
+  gulp.watch(src + '/*.html', browserSync.reload);
+  gulp.watch(src + '/scss/**/*.scss', ['sass']);
+  gulp.watch(src + 'css/app.css', browserSync.reload);
+  gulp.watch(src + '/js/**/*.js', browserSync.reload);
+  gulp.watch(src + '/pages/**/*.+(html|njk|nunjucks)', ['nunjucks']);
+  gulp.watch(src + '/templates/**/*.+(html|njk|nunjucks)', ['nunjucks']);
+})
+
+// optimization
+gulp.task('useref', function(){
+  return gulp.src(src + '/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest(dist))
+});
+
+// clean dist
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+// default
+gulp.task('default', function (callback) {
+  runSequence(['sass', 'nunjucks', 'browserSync', 'watch'],
+    callback
+  )
+})
+
+// build produccion
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
+})
